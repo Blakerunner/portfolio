51efe0970505c49204e1f12889550958b25306f8
Blake Michalzik <bmichalzik@gmail.com>
2021-11-25 21:34:03 -0800

    updates

--- src/mydata.js
+++ src/mydata.js
@@ -28,6 +28,7 @@ import reactIcon from "./images/react.svg"
 import jsIcon from "./images/javascript.svg"
 import designIcon from "./images/design.svg"
 import codeIcon from "./images/code.svg"
+import unicorn from "./images/unicorn.svg"
 
 export default {
   //   Header Details ---------------------
@@ -138,11 +139,17 @@ export default {
 
   // About Secton --------------
   aboutParaOne:
-    "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+    "Hello, I'm Blake and I'm excited to be part of the wildly fascinating computer-based timeline. I'm a recent CS graduate and looking forward to many, many years of those eureka moments when a (code) plan comes together. I believe creative problem solving and engineering holds a special type of mind-stretching joy for most people, but for me, it's my Ikigai.",
   aboutParaTwo:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
+    "I still vividly remember at the age of 4 my first experience with computers. Perched on my father’s knees and wrapped in a towel, from my recent thunderstorm adventures, I was shouting 'Go! Go! Go!'. I was rapidly pressing a spacebar to shoot while father was masterfully navigating our player character through the game level. I was the arms, he was the legs. Years later I learned we were playing Doom 1993.",
   aboutParaThree:
-    "It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+    "Maybe it was the Doom, but years later I joined on to the Australian Defence Force for a decade. I was young and restless and wanted to see what I was capable of. Turns out, I'm capable of anything I aim for. Joining as a Combat Engineer in an Airborne unit, I wanted to be at the front. I learned a lot, how to work as a team, how to take the initiative, fix something before its a problem, completely shift gears because priorities changed and something needs a fix yesterday ... heck yeah let’s go.",
+    aboutParaFour:
+    "Sometime after a particularly ridiculous night-time fast-roping drop from a helicopter into a forest with a chainsaw strapped to my leg, I thought 'Hey I should work on these…",
+    aboutParaFive: 
+    "Avionics Technician was an excellent fit for me, technically complex troubleshooting on aircraft electrical systems was great for me to learn to be particularly methodical and bugfix. There was something missing though, I still really wanted to build things, and I felt that I wasn't contributing enough to humanity. I already knew Tech was the answer. It had always been there with me, even 4 year old me knew it.",
+    aboutParaFive: 
+    "Graduating in 2022, I've got computer science knowledge under my belt and I'm looking for development opportunities. Along with my tenacity, professionalism, and attention to detail, I believe I'll make an excellent team member at your company.",
   aboutImage: blakePortrait1,
 
   //   End About Section ---------------------
